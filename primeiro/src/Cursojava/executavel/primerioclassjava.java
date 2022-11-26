14e099ab2660c056d5df083eff30066cf4490cff
Usuário <Usurio@DESKTOP-7THMSM2>
2022-11-26 17:31:25 -0300

    Crianção do projeto

---
+++ primeiro/src/Cursojava/executavel/primerioclassjava.java
@@ -0,0 +1,15 @@
+package Cursojava.executavel;
+
+public class primerioclassjava {
+	/*Main e um metodo auto execultavel em java*/
+	
+		public static void main(String[] args) {
+			
+
+		/*Escreva texto no console*/
+			
+		System.out.println("primeiro");
+		
+	}
+		
+}
