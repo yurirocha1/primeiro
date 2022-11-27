d7a8c3fec51c20fc8ebd9e96af01b869d14429bd
Usuário <Usurio@DESKTOP-7THMSM2>
2022-11-27 14:30:39 -0300

    aula de cogicos

---
+++ primeiro/src/primeiro/primeiro.java
@@ -0,0 +1,18 @@
+package primeiro;
+
+public class primeiro {
+	
+	
+	public static void main(String[] args) {
+		double nota1 = 11;
+		double nota2 = 50;
+		double nota3 = 500;
+		
+		double mediaFinal = (nota1 + nota2 + nota3 ) /4;
+		
+		System.out.println(" Media das notas = " + mediaFinal);
+	}
+	
+}
+
+
