e3a23284c236419257b3f5540191f966a832f145
Usuário <Usurio@DESKTOP-7THMSM2>
2022-11-26 22:14:11 -0300

    alteração classe pessoa

--- primeiro/src/curso/java/yu/pessoa.java
+++ primeiro/src/curso/java/yu/pessoa.java
@@ -2,4 +2,24 @@ package curso.java.yu;
 
 public class pessoa {
 
+	private String nome;
+	
+	private String sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+	
 }
